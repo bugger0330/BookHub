d7fba0463ab44e8da64821bf0939d747afe2b9eb
junyeoke <junyeoke@kakao.com>
2024-02-22 17:57:27 +0900

    카테고리 수정전

---
+++ src/main/java/com/library/bookhub/repository/PointProductCategoryRepository.java
@@ -0,0 +1,32 @@
+package com.library.bookhub.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.library.bookhub.entity.PointProductCategory;
+
+@Mapper
+public interface PointProductCategoryRepository {
+	
+	
+	// 전체조회 페이징 처리
+	public List<PointProductCategory> findAllWithPagingAndCategoryName(@Param("offset") int offset, @Param("limit") int limit, @Param("categoryName") String categoryName);
+	
+	// 저장
+	public int insert(PointProductCategory pointProductCategory);
+	// 수정
+	public int update(PointProductCategory pointProductCategory);
+	// 삭제
+	public int deleteById(Integer id);
+	// id값이 존재하는지 확인
+	public long existById(int id);
+	// id로 조회
+	public List<PointProductCategory> findById(Integer id);
+	// 총 데이터의 개수 조회
+	public int getCount();
+	
+	
+
+}
---
+++ src/main/java/com/library/bookhub/service/PointProductCategoryService.java
@@ -0,0 +1,82 @@
+package com.library.bookhub.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.library.bookhub.entity.PointProductCategory;
+import com.library.bookhub.repository.PointProductCategoryRepository;
+import com.library.bookhub.web.dto.common.PageReq;
+import com.library.bookhub.web.dto.common.PageRes;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class PointProductCategoryService {
+
+	
+	@Autowired
+	private PointProductCategoryRepository pointProductCategoryRepository;
+
+	public PointProductCategoryService(PointProductCategoryRepository pointProductCategoryRepository) {
+		this.pointProductCategoryRepository = pointProductCategoryRepository;
+	}
+	
+	
+	// 전체조회 - 페이징
+	public PageRes<PointProductCategory> getCategoryWithPaging(PageReq pageReq, String categoryName){
+		int page = pageReq.getPage();
+		int size = pageReq.getSize();
+		int offset = (page - 1) * size;
+		
+		// 총 데이터 개수 조회
+		long totalElements = pointProductCategoryRepository.getCount();
+		
+		// 페이징 된 카테고리 목록 조회
+		List<PointProductCategory> categorys = pointProductCategoryRepository.findAllWithPagingAndCategoryName(offset, offset, categoryName);
+		
+		// 페이징 결과 객체 생성
+		PageRes<PointProductCategory> pageRes = new PageRes<>(categorys, page, totalElements, size);
+		
+		
+		return pageRes;
+		
+	}
+	
+	// 저장
+	public int save(PointProductCategory pointProductCategory) {
+		int queryResult = -1;
+		try {
+			if(pointProductCategory.getCateId() == null) {
+				queryResult = pointProductCategoryRepository.insert(pointProductCategory);
+			} else {
+				// 기본키가 없으면
+				queryResult = pointProductCategoryRepository.update(pointProductCategory);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			log.debug(e.getMessage());
+		}
+		
+		return queryResult;
+	}
+	
+	
+	 // 삭제함수
+    public boolean removeById(int id) {
+    	try {
+			if(pointProductCategoryRepository.existById(id) > 0) {
+				pointProductCategoryRepository.deleteById(id);
+				return true;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			log.debug(e.getMessage());
+		}
+    	return false;
+    }
+	
+	
+}
