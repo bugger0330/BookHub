3282a4361be3d048400daa64b720dfba9f601492
bugger0330 <buggert0330@naver.com>
2024-03-07 09:54:35 +0900

    error 페이지 만듬

---
+++ src/main/java/com/library/bookhub/handler/CustomErrorController.java
@@ -0,0 +1,36 @@
+package com.library.bookhub.handler;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * @fileName : CustomErrorController.java
+ * @Project : BookHub
+ * @Date : 2024. 3. 7.
+ * @작성자 : 강민
+ * @설명 : 
+ */
+@Controller
+public class CustomErrorController implements ErrorController {
+	
+	@GetMapping("/error")
+	public String errorPage(HttpServletRequest request) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if(status != null){
+            int statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+            	return "error";
+            } 
+        }
+		return null;
+	}
+	
+
+}
